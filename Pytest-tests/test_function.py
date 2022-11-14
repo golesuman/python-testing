19b5bbee0239ad1033377d6a56397917efd486fb
Suman Gole <suman.gole@truenary.com>
2022-11-14 10:27:04 +0545

    add: fixtures in test_function

--- Pytest-tests/test_function.py
+++ Pytest-tests/test_function.py
@@ -6,7 +6,7 @@ def func_sqrt(x):
     return math.sqrt(x)
 
 
-@pytest.mark.filterwarnings
+@pytest.mark.number
 def test_sqrt():
     x = 25
     value = func_sqrt(x)
@@ -19,7 +19,23 @@ def check_str(x):
     return True if type(x) == str else None
 
 
-@pytest.mark.filterwarnings
+@pytest.mark.strings
 def test_str_type():
     a = check_str('5')
     assert a == True
+
+
+
+@pytest.fixture
+def input_value():
+    input_ = 39
+    return input_
+
+
+def test_divisible_by_3(input_value):
+    assert input_value % 3 == 0
+
+def test_divisible_by_6(input_value):
+    assert input_value % 6 == 0
+
+
